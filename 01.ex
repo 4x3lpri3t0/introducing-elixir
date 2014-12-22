67d400bdf09a591844247077a1ede329d0c406e7
Zedronar <prietoaxel@gmail.com>
2014-12-22 18:26:49 -0300

    playing with Elixir

--- 01.ex
+++
@@ -1,9 +0,0 @@
-# THE &
-
-fall_velocity= fn (distance) -> :math.sqrt(2 * 9.8 * distance) end
-#Fun<erl_eval.6.111823515>
-
-# If that is too verbose for you, you could use the &:
-
-fall_velocity= &(:math.sqrt(2 * 9.8 * &1))
-
---
+++ drop.ex
@@ -0,0 +1,29 @@
+# THE &
+
+fall_velocity= fn (distance) -> :math.sqrt(2 * 9.8 * distance) end
+#Fun<erl_eval.6.111823515>
+
+# If that is too verbose for you, you could use the &:
+
+fall_velocity= &(:math.sqrt(2 * 9.8 * &1))
+
+# DEFININE MODULES
+
+defmodule Drop do
+  def fall_velocity(distance) do
+    :math.sqrt(2 * 9.8 * distance)
+  end
+
+  def mps_to_mph(mps) do
+    2.23693629 * mps
+  end
+
+  def mps_to_kph(mps) do
+    3.6 * mps
+  end
+end
+
+# Call from console: Drop.fall_velocity 1
+
+# Use defp instead of def for private functions
+
