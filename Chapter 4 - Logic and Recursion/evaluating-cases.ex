41e55bd5e6ebc0b7ef8130f51a52d9cc1d289d32
Zedronar <prietoaxel@gmail.com>
2015-01-06 16:46:52 -0300

    small progress

---
+++ Chapter 4 - Logic and Recursion/evaluating-cases.ex
@@ -0,0 +1,13 @@
+defmodule Drop do
+
+  def fall_velocity(planemo, distance) when distance >= 0 do
+    case planemo do
+      :earth -> :math.sqrt(2 * 9.8 * distance)
+      :moon  -> :math.sqrt(2 * 1.6 * distance)
+      :mars  -> :math.sqrt(2 * 3.71 * distance)
+    end
+  end
+
+end
+
+# Each matching value is followed by a ->, which you can read as “yields”
