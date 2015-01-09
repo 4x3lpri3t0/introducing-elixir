22df1f7eda519560147a91a0e25b769e4611842d
Zedronar <prietoaxel@gmail.com>
2015-01-09 18:42:22 -0300

    minor changes

---
+++ Chapter 4 - Logic and Recursion/factorial-counting-up.ex
@@ -0,0 +1,4 @@
+# You can achieve tail recursion for factorials by applying the counting-up approach to
+# factorials. You’ll get the same results (at least for integer values), but the calculations
+# will work a little differently:
+
